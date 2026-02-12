a9587500a59d5b00fd06b3d9cd75e3bd3957b9a0
RikkiGenack <rgenack@student.touro.edu>
2026-02-12 17:39:28 -0500

    Finished!

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, current_app, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def welcome_endpoint():
+    return "<h1>Welcome to My Flask API!</h1>"
+
+
+@app.route("/about", methods=["GET"])
+def about_endpoint():
+    return jsonify({"name": "Rikki Genack", "course":
+        "MCON-357 - Backend Development", "semester":
+        "Spring 2026"})
+
+@app.route("/greet/<name>", methods=["GET"])
+def greet_endpoint(name):
+    return f"Hello, {name}! Welcome to Flask."
+
+
+
+@app.route("/calculate", methods=["GET"])
+def calculate_endpoint(num1, num2, operation):
+    if request.args.get('operation') == "add":
+        result = num1 + num2
+    if request.args.get('operation') == "subtract":
+        result = num1 - num2
+    if request.args.get('operation') == "multiply":
+        result = num1 * num2
+    elif request.args.get('operation') == "divide":
+        result = num1 / num2
+    return jsonify({"result": result}, {"operation": operation})
+
+@app.route("/echo", methods=["POST"])
+def echo_endpoint():
+    print(request.get_json())
+    return f'{request.get_json()} echoed": true'
+
+@app.route("/status/<int:code>", methods=["GET"])
+def status_endpoint(code):
+    return ((f'This is a {code} error'), code)
+
+
+
+
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
